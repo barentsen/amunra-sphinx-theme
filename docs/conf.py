c8841d4900d29d623bae268fed09fe215a46ab69
Geert Barentsen <geert@barentsen.be>
2019-08-26 21:13:38 -0700

    Add preview image

--- docs/conf.py
+++ docs/conf.py
@@ -16,6 +16,8 @@ rawfiles = ['.nojekyll']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
+html_static_path = ['_static']
+
 html_theme = 'amunra_sphinx_theme'
 
 html_theme_options = {
