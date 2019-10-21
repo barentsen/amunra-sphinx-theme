c54c04b5189e2032d8d70ddd4261e2d0b124bb63
Geert Barentsen <geert@barentsen.be>
2019-10-21 15:34:14 -0700

    Mark the extension as safe for parallel reading/writing

--- amunra_sphinx_theme/__init__.py
+++ amunra_sphinx_theme/__init__.py
@@ -10,3 +10,6 @@ def get_html_theme_path():
 # See http://www.sphinx-doc.org/en/stable/theming.html#distribute-your-theme-as-a-python-package
 def setup(app):
     app.add_html_theme('amunra_sphinx_theme', path.abspath(path.dirname(__file__)))
+    return {"version": __version__,
+            "parallel_read_safe": True,
+            "parallel_write_safe": True}
